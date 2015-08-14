dfcbff9ffa52e2689d6889dd93830492a6d64484
Chelsea Voss <chelsea@khanacademy.org>
2015-08-13 22:37:14 -0700

    2 spaces -> 4 spaces

--- prisoners_dilemma.js
+++ prisoners_dilemma.js
@@ -19,7 +19,7 @@ if (Meteor.isClient) {
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    Meteor.startup(function () {
+        // code to run on server at startup
+    });
 }
