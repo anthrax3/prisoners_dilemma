9b92f2339db413673d06b8f5ea29ba5405ec966a
Chelsea Voss <chelsea@khanacademy.org>
2015-08-13 23:21:36 -0700

    Add local testing against CooperateBot, DefectBot, and yourself

--- prisoners_dilemma.js
+++ prisoners_dilemma.js
@@ -30,20 +30,71 @@ function payoff(myChoice, theirChoice) {
     }
 }
 
+function compete(me, them, numRounds) {
+    // Compete me against them, over numRounds rounds.
+    // Return the total payoff to both me and them.
+    var payoffToMe = 0;
+    var payoffToThem = 0;
+
+    var myLastChoice = null;
+    var themLastChoice = null;
+
+    var myState = null;
+    var themState = null;
+
+    for (var i=0; i<numRounds; i++) {
+        var myNext = me(myLastChoice, myState);
+        var themNext = them(themLastChoice, themState);
+
+        themState = themNext.state;
+        themLastChoice = themNext.choice;
+        myState = myNext.state;
+        myLastChoice = myNext.choice;
+
+        payoffToMe += payoff(myLastChoice, themLastChoice);
+        payoffToThem += payoff(themLastChoice, myLastChoice);
+    }
+
+    return {
+        'me': payoffToMe,
+        'them': payoffToThem,
+    }
+}
+
 if (Meteor.isClient) {
 
     Session.setDefault('cooperateBotStats', 'No data yet.');
+    Session.setDefault('defectBotStats', 'No data yet.');
+    Session.setDefault('yourselfBotStats', 'No data yet.');
 
-    Template.cooperateBotStats.helpers({
+    Template.testingStats.helpers({
         cooperateBotStats: function() {
             return Session.get('cooperateBotStats');
-        }
+        },
+        defectBotStats: function() {
+            return Session.get('defectBotStats');
+        },
+        yourselfBotStats: function() {
+            return Session.get('yourselfBotStats');
+        },
     });
 
     Template.body.events({
         'click button': function(e) {
             var userCode = $('textarea#userCode').val();
-            Session.set('cooperateBotStats', userCode);
+
+            var me = cooperateBot; //TODO -- put my bot here, using userCode
+            var numRounds = 20;
+
+            var cooperatePayoffs = compete(me, cooperateBot, numRounds);
+            Session.set('cooperateBotStats', 'You earned '+cooperatePayoffs.me+' and CooperateBot earned '+cooperatePayoffs.them+' over '+numRounds+' rounds.');
+
+            var defectPayoffs = compete(me, defectBot, numRounds);
+            Session.set('defectBotStats', 'You earned '+defectPayoffs.me+' and DefectBot earned '+defectPayoffs.them+' over '+numRounds+' rounds.');
+
+            var yourselfPayoffs = compete(me, me, numRounds);
+            Session.set('yourselfBotStats', 'Playing against yourself, You #1 earned '+yourselfPayoffs.me+' and You #2 earned '+yourselfPayoffs.them+' over '+numRounds+' rounds.');
+
         }
     });
 
